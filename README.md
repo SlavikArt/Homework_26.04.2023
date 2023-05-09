66dca6d121387c8bfa5dfdc3c8ba4b53a5353c9c
Vyacheslav <52282118+SlavikArt@users.noreply.github.com>
2023-05-09 18:58:33 +0300

    Добавьте файлы проекта.

---
+++ README.md
@@ -0,0 +1,13 @@
+
+# Homework_26.04.2023
+Д/з: list
+
+<a href="https://gist.github.com/SlavikArt/efc6323d5e229dc51193fbea86a5a8b2">Gist - страница со всеми кодами проектов</a>
+
+* [main](main)
+
+<p align="center">
+    <h2>Main</h2>
+    <p></p>
+    <img src="images/main.png">
+</p>
---
+++ main/main.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <list>
+#include "Point.h"
+using namespace std;
+
+int main()
+{
+	srand(time(0));
+
+	cout << "list<Point<int>>:\n";
+
+	list<Point<int>> lst;
+
+	for (int i = 0; i < 5; i++)
+	{
+		lst.push_back(
+			Point<int>(
+				rand() % 10,
+				rand() % 10,
+				rand() % 10
+			)
+		);
+	}
+
+	for (const auto& point: lst)
+	{
+		cout << point << "\n";
+	}
+	cout << "\n";
+
+	cout << "list<list<Point<int>>>:\n";
+
+	list<list<Point<int>>> lst2;
+
+	for (int i = 0; i < 3; i++)
+	{
+		list<Point<int>> tmp;
+
+		for (int i = 0; i < 5; i++)
+		{
+			tmp.push_back(
+				Point<int>(
+					rand() % 10,
+					rand() % 10,
+					rand() % 10
+				)
+			);
+		}
+		lst2.push_back(tmp);
+	}
+
+	for (const auto& subList : lst2)
+	{
+		for (const auto& point : subList)
+		{
+			cout << point << ", ";
+		}
+		cout << "\n";
+	}
+}
